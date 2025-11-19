bd7a0ace87fb65a13218994de2fa83cbc2a881a6
youvegotnigel <nigeldave19@gmail.com>
2025-11-19 12:24:10 +0530

    added valid css selectors

--- login_test.ts
+++ login_test.ts
@@ -3,9 +3,9 @@ Feature('login');
 Scenario('test something',  ({ I }) => {
 
     I.amOnPage('/');
-    I.fillField('username', 'standard_user');
-    I.fillField('password', 'secret_sauce');
-    I.click('Login');
+    I.fillField('[data-test=username]', 'standard_user');
+    I.fillField('[data-test=password]', 'secret_sauce');
+    I.click('[data-test=login-button]');
     I.see('Products'); 
 
 });
