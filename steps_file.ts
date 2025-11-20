3d147f4b7186cafd175ed700ccf7083aba3f6144
youvegotnigel <nigeldave19@gmail.com>
2025-11-20 21:23:29 +0530

    added new actor to login

--- steps_file.ts
+++ steps_file.ts
@@ -1,10 +1,20 @@
 // in this file you can append custom step methods to 'I' object
 
+const { I } = inject();
+
 export = function() {
   return actor({
 
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
+
+    login(username: string, password: string) {
+      I.fillField('[data-test="username"]', username);
+      I.fillField('[data-test="password"]', password);
+      I.click('[data-test="login-button"]');
+      
+      I.see('Products'); 
+    }
 
   });
 }
