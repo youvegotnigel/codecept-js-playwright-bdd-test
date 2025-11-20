39b1e094bfcfbc44dd0bcee456a4a290b44a4c30
Nigel <nigeldave19@gmail.com>
2025-11-20 23:55:56 +0530

    added price sorting test

--- tests/product_test.ts
+++ tests/product_test.ts
@@ -1,3 +1,5 @@
+import assert from 'assert';
+
 Feature('product');
 
 Scenario('verify adding items to cart',  ({ I }) => {
@@ -10,5 +12,26 @@ Scenario('verify adding items to cart',  ({ I }) => {
     I.click({ byTestId: 'add-to-cart-sauce-labs-fleece-jacket' });
 
     I.see('3', { byTestId: 'shopping-cart-badge' });
+
+});
+
+
+Scenario.only('verify price sorting',  ({ I }) => {
+
+    I.amOnPage('/');
+    I.login("standard_user", "secret_sauce");
+
+    I.selectOption({ byTestId: 'product-sort-container' }, 'Price (low to high)');
+
+    const uiPricesText: string[] = await I.grabTextFromAll('.inventory_item_price');
+    const uiPrices = uiPricesText.map((t) => parseFloat(t.replace('$', '')));
+
+    const sortedPrices = [...uiPrices].sort((a, b) => a - b);
+
+    assert.deepStrictEqual(
+        uiPrices,
+        sortedPrices,
+        'Items are not sorted by price low → high',
+    );
 
 });
