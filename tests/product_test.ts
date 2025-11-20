1f990c172e17c41999a0ae6026db7baf59b2f7f0
youvegotnigel <nigeldave19@gmail.com>
2025-11-20 22:18:23 +0530

    use custom locator strategy

--- tests/product_test.ts
+++ tests/product_test.ts
@@ -5,10 +5,10 @@ Scenario('verify adding items to cart',  ({ I }) => {
     I.amOnPage('/');
     I.login("standard_user", "secret_sauce");
     
-    I.click('[data-test="add-to-cart-sauce-labs-backpack"]');
-    I.click('[data-test="add-to-cart-sauce-labs-bike-light"]');
-    I.click('[data-test="add-to-cart-sauce-labs-fleece-jacket"]');
+    I.click({ byTestId: 'add-to-cart-sauce-labs-backpack' });
+    I.click({ byTestId: 'add-to-cart-sauce-labs-bike-light' });
+    I.click({ byTestId: 'add-to-cart-sauce-labs-fleece-jacket' });
 
-    I.see('3', '[data-test="shopping-cart-badge"]');
+    I.see('3', { byTestId: 'shopping-cart-badge' });
 
 });
