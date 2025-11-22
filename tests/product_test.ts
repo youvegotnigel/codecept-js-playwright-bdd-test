21d52cdb9953c3cf929842296885e064f8d92c80
Nigel <nigeldave19@gmail.com>
2025-11-22 07:45:24 +0530

    added new test - verify price sorting high to low

--- tests/product_test.ts
+++ tests/product_test.ts
@@ -16,7 +16,7 @@ Scenario('verify adding items to cart',  ({ I }) => {
 });
 
 
-Scenario.only('verify price sorting', async ({ I }) => {
+Scenario('verify price sorting low to high', async ({ I }) => {
 
     await I.amOnPage('/');
     await I.login("standard_user", "secret_sauce");
@@ -29,14 +29,41 @@ Scenario.only('verify price sorting', async ({ I }) => {
 
     const sortedPrices = [...uiPrices].sort((a, b) => a - b);
 
-    console.log('UI Text Prices: ', uiPricesText);
-    console.log('UI Prices: ', uiPrices);
-    console.log('Sorted Prices: ', sortedPrices);
+    console.log('UI Text Prices:', uiPricesText);
+    console.log('UI Prices:', uiPrices);
+    console.log('Sorted Prices:', sortedPrices);
 
     assert.deepStrictEqual(
         uiPrices,
         sortedPrices,
         'Items are not sorted by price low → high',
+    );
+
+});
+
+
+Scenario('verify price sorting high to low', async ({ I }) => {
+
+    await I.amOnPage('/');
+    await I.login("standard_user", "secret_sauce");
+
+    await I.selectOption({ byTestId: 'product-sort-container' }, 'Price (high to low)');
+
+    //const uiPricesText: string[] = await I.grabTextFromAll({ byTestId: 'inventory-item-price' });
+    const uiPricesText: string[] = await I.grabTextFromAll('.inventory_item_price');
+    const uiPrices = uiPricesText.map((t) => parseFloat(t.replace('$', '')));
+
+    //create a DESCENDING sorted copy to compare against (high -> low)
+    const sortedPrices = [...uiPrices].sort((a, b) => b - a);
+
+    console.log('UI Text Prices:', uiPricesText);
+    console.log('UI Prices:', uiPrices);
+    console.log('Sorted Prices:', sortedPrices);
+
+    assert.deepStrictEqual(
+        uiPrices,
+        sortedPrices,
+        'Items are not sorted by price high → low',
     );
 
 });
